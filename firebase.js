8704db130676cf626e25acd6786dff116ab49e21
minebotweb <uniaostudiosnetwork@gmail.com>
2025-06-21 16:52:59 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Configuração do Firebase com seu projeto real
+const firebaseConfig = {
+  apiKey: "AIzaSyC_z8UhVlp17sL6RLBiYqwwU_4g-c_uByg",
+  authDomain: "cubescape-account.firebaseapp.com",
+  projectId: "cubescape-account",
+  storageBucket: "cubescape-account.firebasestorage.app",
+  messagingSenderId: "63348118324",
+  appId: "1:63348118324:web:c69420a5e9b05b3596cb61",
+  measurementId: "G-F70R3VNY2L"
+};
+firebase.initializeApp(firebaseConfig);
